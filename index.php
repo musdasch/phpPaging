a3d5bfaecfaafc9b567a007c9add9da3e1282a66
Tim <voegtli@projektvenice.ch>
2015-11-03 13:09:59 +0100

    init project.

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+error_reporting( E_ALL );
+ini_set("display_errors", 1);
+
+$mysqli = new mysqli( 'localhost', 'root', 'nas3o1818', 'pagination' );
+
+if ( mysqli_connect_errno() ) {
+    printf( "Connect failed: %s\n", mysqli_connect_error() );
+    exit();
+}
+
+//$stmt = $mysqli->prepare( "INSERT INTO users VALUES ( NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )" );
+
+$stmt = $mysqli->prepare( "SELECT * FROM pagination LIMIT 10" );
+
+/*$stmt->bind_param( 
+	'ssssssssss',
+	$inDB[ 'firstname' ],
+	$inDB[ 'latname' ],
+	$inDB[ 'street' ],
+	$inDB[ 'plz' ],
+	$inDB[ 'city' ],
+	$inDB[ 'country' ],
+	$inDB[ 'username' ],
+	$inDB[ 'password' ],
+	$inDB[ 'email' ],
+	$inDB[ 'phone' ]
+);*/
+
+$stmt->execute()
+
+//$stmt->bind_result( $district );
+
+//print_r( $stmt->fetch() );
+
+//print_r( $district );
+
+//$stmt->close();
+
+?>
+<!doctype html>
+<html lang="de">
+	<head>
+		<meta charset="de_CH.UTF-8"/>
+		<title>Paging</title>
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/foundation.min.css">
+	</head>
+	<body>
+		<div class="row">
+			<div class="medium-12 columns">
+				<h1>Paging</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="medium-12 columns">
+				<table>
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th width="800">und so.</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>1</td>
+							<td></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<div class="row">
+			<div class="medium-12 columns">
+				<a href="#" class="button radius"><</a>
+				<a href="#" class="button radius">></a>
+			</div>
+		</div>
+		<script type="text/javascript" src="js/foundation.min.js"></script>
+	</body>
+</html>
---
+++ sql/pagination.sql
@@ -0,0 +1,84 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Erstellungszeit: 04. Dez 2014 um 10:34
+-- Server Version: 5.6.12
+-- PHP-Version: 5.5.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Datenbank: `pagination`
+--
+CREATE DATABASE IF NOT EXISTS `pagination` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `pagination`;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `pagination`
+--
+
+DROP TABLE IF EXISTS `pagination`;
+CREATE TABLE IF NOT EXISTS `pagination` (
+  `counter` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Daten für Tabelle `pagination`
+--
+
+INSERT INTO `pagination` (`counter`) VALUES
+(1),
+(2),
+(3),
+(4),
+(5),
+(6),
+(7),
+(8),
+(9),
+(10),
+(11),
+(12),
+(13),
+(14),
+(15),
+(16),
+(17),
+(18),
+(19),
+(20),
+(21),
+(22),
+(23),
+(24),
+(25),
+(26),
+(27),
+(28),
+(29),
+(30),
+(31),
+(32),
+(33),
+(34),
+(35),
+(36),
+(37),
+(38),
+(39),
+(40);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
